54b818f81c51f3dc987914442775c11f961e3b25
Marcos Vinicius Lima de França <mfranca@nelogica.com.br>
2022-07-08 16:16:17 -0300

    Criando Component MenuLateral

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <main class="columns is-gapless is-multiline">
+    <div class="column is-one-quarter"><MenuLateral /></div>
+    <div class="column is-three-quarter"></div>
+  </main>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { defineComponent } from "vue";
+import MenuLateral from "./components/MenuLateral.vue";
 
 export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: "App",
+  components: { MenuLateral },
 });
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ src/components/MenuLateral.vue
@@ -0,0 +1,28 @@
+<template>
+  <header>
+    <h1><img src="../assets/logo.png" alt="Logo do Vini Tracker" /></h1>
+  </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "MenuLateral",
+});
+</script>
+
+<style scoped>
+header {
+  background: #0d3b66;
+  width: 100%;
+  height: 100vh;
+}
+
+@media only screen and (max-width: 768px) {
+  header {
+    padding: 2.5rem;
+    height: auto;
+  }
+}
+</style>
