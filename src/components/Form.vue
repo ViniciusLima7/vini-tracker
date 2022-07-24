ca9e3fc6731651489ac6836644b9039b2c4105ba
Marcos Vinicius Lima de França <mfranca@nelogica.com.br>
2022-07-24 11:11:23 -0300

    refactor: Adiciona Compónent Temporizador

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -14,25 +14,7 @@
       </div>
 
       <div class="column">
-        <div
-          class="is-flex is-align-items-center is-justify-content-space-between"
-        >
-          <Cronometro :timeInSeconds="timeInSeconds" />
-
-          <button class="button" @click="iniciar">
-            <span class="icon">
-              <i class="fas fa-play"></i>
-            </span>
-            <span>Start</span>
-          </button>
-
-          <button class="button" @click="finalizar">
-            <span class="icon">
-              <i class="fas fa-stop"></i>
-            </span>
-            <span>Fim</span>
-          </button>
-        </div>
+        <Temporizador />
       </div>
     </div>
   </div>
@@ -40,39 +22,12 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import Cronometro from "./Cronometro.vue";
+import Temporizador from "./Temporizador.vue";
 
 export default defineComponent({
-  name: "Formulario",
+  name: "Form",
   components: {
-    Cronometro,
-  },
-  data() {
-    return {
-      timeInSeconds: 0,
-      cronometro: 0,
-    };
-  },
-  methods: {
-    /**
-     * Inicia o contador em segundos
-     *@description
-     *10/07/2022 vlima Inicia Contador e guarda a referencia na variavel cronometro
-     */
-    iniciar() {
-      //Começar a Contar
-      this.cronometro = setInterval(() => {
-        this.timeInSeconds++;
-      }, 1000);
-    },
-    /**
-     * Finaliza o contador em segundos
-     *@description
-     *10/07/2022 vlima Finaliza Contador
-     */
-    finalizar() {
-      clearInterval(this.cronometro);
-    },
+    Temporizador,
   },
 });
 </script>
---
+++ src/components/Temporizador.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="is-flex is-align-items-center is-justify-content-space-between">
+    <Cronometro :timeInSeconds="timeInSeconds" />
+
+    <button class="button" @click="iniciar">
+      <span class="icon">
+        <i class="fas fa-play"></i>
+      </span>
+      <span>Start</span>
+    </button>
+
+    <button class="button" @click="finalizar">
+      <span class="icon">
+        <i class="fas fa-stop"></i>
+      </span>
+      <span>Fim</span>
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Cronometro from "./Cronometro.vue";
+
+export default defineComponent({
+  name: "Temporizador",
+  components: {
+    Cronometro,
+  },
+  data() {
+    return {
+      timeInSeconds: 0,
+      cronometro: 0,
+    };
+  },
+  methods: {
+    /**
+     * Inicia o contador em segundos
+     *@description
+     *10/07/2022 vlima Inicia Contador e guarda a referencia na variavel cronometro
+     */
+    iniciar() {
+      //Começar a Contar
+      this.cronometro = setInterval(() => {
+        this.timeInSeconds++;
+      }, 1000);
+    },
+    /**
+     * Finaliza o contador em segundos
+     *@description
+     *10/07/2022 vlima Finaliza Contador
+     */
+    finalizar() {
+      clearInterval(this.cronometro);
+    },
+  },
+});
+</script>
