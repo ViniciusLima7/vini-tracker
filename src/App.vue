befe4961372a32058affcd336a2e60a0cc1b1cdb
Marcos Vinicius Lima de França <mfranca@nelogica.com.br>
2022-07-25 18:32:49 -0300

    refactor: Adiciona Compónent Tarefa

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,12 @@
     </div>
     <div class="column is-three-quarter">
       <Form></Form>
+      <div class="lista">
+        <Tarefa />
+        <Tarefa />
+        <Tarefa />
+        <Tarefa />
+      </div>
     </div>
   </main>
 </template>
@@ -13,11 +19,16 @@
 import { defineComponent } from "vue";
 import MenuLateral from "./components/MenuLateral.vue";
 import Form from "./components/Form.vue";
+import Tarefa from "./components/Tarefa.vue";
 
 export default defineComponent({
   name: "App",
-  components: { MenuLateral, Form },
+  components: { MenuLateral, Form, Tarefa },
 });
 </script>
 
-<style></style>
+<style>
+.lista {
+  padding: 1.25rem;
+}
+</style>
---
+++ src/components/Tarefa.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="box has-text-weight-bold">
+    <div class="columns">
+      <div class="column is-7">Descrição da Tarefa</div>
+      <div class="column">
+        <Cronometro :timeInSeconds="10" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Cronometro from "./Cronometro.vue";
+
+export default defineComponent({
+  name: "Tarefa",
+  components: { Cronometro },
+});
+</script>
+
+<style scoped>
+.box {
+  background: #faf0ca;
+}
+</style>
