99666bf1726fb33cf0f61edc0c60a83d568fe144
Marcos Vinicius Lima de França <mfranca@nelogica.com.br>
2022-08-02 21:12:56 -0300

    feat: Adiciona Navegação entre Rotas

--- src/components/MenuLateral.vue
+++ src/components/MenuLateral.vue
@@ -1,12 +1,30 @@
 <template>
   <header>
-    <h1>
+    <h1 class="has-text-centered">
       <img src="../assets/vini-trackerlogov2.png" alt="Logo do Vini Tracker" />
     </h1>
-    <button class="button" @click="toggleTema">
-      <img class="theme" src="../assets/themeOn.svg" alt="" />
-      {{ textButton }}
-    </button>
+    <div class="has-text-centered">
+      <button class="button" @click="toggleTema">
+        <img class="theme" src="../assets/themeOn.svg" alt="" />
+        {{ textButton }}
+      </button>
+    </div>
+    <nav class="panel mt-5">
+      <ul>
+        <li>
+          <router-link to="/" class="link">
+            <i class="fas fa-tasks"> </i>
+            tarefas
+          </router-link>
+        </li>
+        <li>
+          <router-link to="/projetos" class="link">
+            <i class="fas fa-project-diagram"> </i>
+            projetos
+          </router-link>
+        </li>
+      </ul>
+    </nav>
   </header>
 </template>
 
@@ -54,7 +72,6 @@ header {
   background: var(--bg-header);
   width: 100%;
   height: 100vh;
-  text-align: center;
 }
 
 .button {
@@ -73,5 +90,21 @@ header {
     padding: 2.5rem;
     height: auto;
   }
+}
+
+.panel li {
+  margin: 8px 0;
+}
+
+.link {
+  color: #fff;
+}
+
+.link:hover {
+  color: #faf0ca;
+}
+
+.link.router-link-active {
+  color: #faf0ca;
 }
 </style>
