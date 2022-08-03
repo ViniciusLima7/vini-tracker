845334919d3d57b592e5109611a844084adb417d
Marcos Vinicius Lima de França <mfranca@nelogica.com.br>
2022-08-02 22:02:39 -0300

    feat: Adiciona funcionalidade de Salvar Projeto em Lista

--- src/views/Projetos.vue
+++ src/views/Projetos.vue
@@ -1,11 +1,70 @@
 <template>
-  <h1>Projetos</h1>
+  <section class="projetos">
+    <h1 class="title">Projetos</h1>
+    <form @submit.prevent="salvar">
+      <div class="field">
+        <label for="nomeDoProjeto" class="label"> Nome do Projeto </label>
+        <input
+          type="text"
+          class="input"
+          v-model="nomeDoProjeto"
+          id="nomeDoProjeto"
+        />
+      </div>
+      <div class="field">
+        <button class="button" type="submit">Salvar</button>
+      </div>
+    </form>
+    <table class="table is-fullwidth">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Nome</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="projeto in projetos" :key="projeto.id">
+          <th>{{ projeto.id }}</th>
+          <th>{{ projeto.name }}</th>
+        </tr>
+      </tbody>
+    </table>
+  </section>
 </template>
 
 <script lang="ts">
+import IProjeto from "../interfaces/IProjeto";
 import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "Projetos",
+  data() {
+    return {
+      nomeDoProjeto: "",
+      projetos: [] as IProjeto[],
+    };
+  },
+  methods: {
+    /**
+     * Salva o Projeto digitado na Lista de Projetos e depois limpa o input
+     *@description
+     *02/08/2022 vlima Salvar Projeto
+     */
+    salvar() {
+      const projeto: IProjeto = {
+        name: this.nomeDoProjeto,
+        id: new Date().toISOString(),
+      };
+
+      this.projetos.push(projeto);
+      this.nomeDoProjeto = "";
+    },
+  },
 });
 </script>
+
+<style scoped>
+.projetos {
+  padding: 1.25rem;
+}
+</style>
