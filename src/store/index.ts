174a7bb8806c26ea84db1ad59c1f5568610d577e
Marcos Vinicius Lima de França <mfranca@nelogica.com.br>
2022-08-04 19:44:13 -0300

    feat: Adiciona Store

---
+++ src/store/index.ts
@@ -0,0 +1,19 @@
+import IProjeto from "@/interfaces/IProjeto";
+import { InjectionKey } from "vue";
+import { createStore, Store } from "vuex";
+
+interface State {
+  projetos: IProjeto;
+}
+
+export const key: InjectionKey<Store<State>> = Symbol();
+
+export const store = createStore<State>({
+  state: {
+    projetos: [
+      { id: new Date().toISOString(), name: "Nelogica" },
+      { id: new Date().toISOString(), name: "React" },
+      { id: new Date().toISOString(), name: "Vue" },
+    ],
+  },
+});
